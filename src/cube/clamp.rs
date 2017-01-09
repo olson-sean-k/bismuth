5f964797f566a5257e56a0c55ca51dbcb7e5a45a
Sean Olson <olson.sean.k@gmail.com>
2017-01-09 11:55:10 -0800

    Implement operators for inner type.

--- src/cube/clamp.rs
+++ src/cube/clamp.rs
@@ -31,6 +31,10 @@ impl<T, R> Clamped<T, R>
     pub fn min_value() -> Self {
         Clamped(R::min_value(), PhantomData)
     }
+
+    pub fn unclamped(&self) -> T {
+        self.0
+    }
 }
 
 impl<T, R> Clone for Clamped<T, R>
@@ -97,6 +101,17 @@ impl<T, R> ops::Add for Clamped<T, R>
     }
 }
 
+impl<T, R> ops::Add<T> for Clamped<T, R>
+    where T: ops::Add<Output = T> + Copy + Num + cmp::PartialOrd,
+          R: ClampedRange<Output = T>
+{
+    type Output = Self;
+
+    fn add(self, other: T) -> Self::Output {
+        Self::new(self.0.add(other))
+    }
+}
+
 impl<T, R> ops::Deref for Clamped<T, R>
     where T: Copy + Num + cmp::PartialOrd,
           R: ClampedRange<Output = T>
@@ -119,6 +134,17 @@ impl<T, R> ops::Div for Clamped<T, R>
     }
 }
 
+impl<T, R> ops::Div<T> for Clamped<T, R>
+    where T: Copy + ops::Div<Output = T> + Num + cmp::PartialOrd,
+          R: ClampedRange<Output = T>
+{
+    type Output = Self;
+
+    fn div(self, other: T) -> Self::Output {
+        Self::new(self.0.div(other))
+    }
+}
+
 impl<T, R> ops::Mul for Clamped<T, R>
     where T: Copy + ops::Mul<Output = T> + Num + cmp::PartialOrd,
           R: ClampedRange<Output = T>
@@ -127,6 +153,17 @@ impl<T, R> ops::Mul for Clamped<T, R>
 
     fn mul(self, other: Self) -> Self::Output {
         Self::new(self.0.mul(other.0))
+    }
+}
+
+impl<T, R> ops::Mul<T> for Clamped<T, R>
+    where T: Copy + ops::Mul<Output = T> + Num + cmp::PartialOrd,
+          R: ClampedRange<Output = T>
+{
+    type Output = Self;
+
+    fn mul(self, other: T) -> Self::Output {
+        Self::new(self.0.mul(other))
     }
 }
 
@@ -149,5 +186,16 @@ impl<T, R> ops::Sub for Clamped<T, R>
 
     fn sub(self, other: Self) -> Self::Output {
         Self::new(self.0.sub(other.0))
+    }
+}
+
+impl<T, R> ops::Sub<T> for Clamped<T, R>
+    where T: Copy + Num + ops::Sub<Output = T> + cmp::PartialOrd,
+          R: ClampedRange<Output = T>
+{
+    type Output = Self;
+
+    fn sub(self, other: T) -> Self::Output {
+        Self::new(self.0.sub(other))
     }
 }
