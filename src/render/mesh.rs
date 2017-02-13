08ade72f62e9d4153408f250d99062f8995b9113
Sean Olson <olson.sean.k@gmail.com>
2017-02-13 13:29:12 -0800

    Parallelize generation of mesh buffers.

--- src/render/mesh.rs
+++ src/render/mesh.rs
@@ -1,5 +1,5 @@
 use OptionExt;
-use cube::{CubeRef, Spatial};
+use cube::{CubeRef, Join, Spatial};
 use math::{IntoSpace, FScalar, FVector3};
 use mesh::{DecomposePolygon, DecomposePrimitive, MapPrimitive, Triangle, UCube};
 use super::pipeline::{Color, ColorExt, Index, Vertex};
@@ -40,37 +40,48 @@ impl MeshBuffer {
     }
 }
 
+impl Default for MeshBuffer {
+    fn default() -> Self {
+        MeshBuffer::new()
+    }
+}
+
+impl Join for MeshBuffer {
+    fn join(mut self, mut buffer: Self) -> Self {
+        self.append(&mut buffer);
+        self
+    }
+}
+
 pub trait Mesh {
     fn mesh_buffer(&self) -> MeshBuffer;
 }
 
 impl<'a> Mesh for CubeRef<'a> {
     fn mesh_buffer(&self) -> MeshBuffer {
-        let mut buffer = MeshBuffer::new();
-        for cube in self.iter() {
+        self.for_each(|cube| {
+            let mut buffer = MeshBuffer::new();
             if let Some(leaf) = cube.as_leaf().and_if(|leaf| !leaf.geometry.is_empty()) {
                 let origin: FVector3 = cube.partition().origin().coords.into_space();
                 let width = cube.partition().width().exp() as FScalar;
-                let mut local = MeshBuffer::new();
-                local.extend(UCube::with_unit_width()
-                                 .map_points(|point| leaf.geometry.map_unit_cube_point(&point))
-                                 .map_points(|point| (point * width) + origin)
-                                 .triangulate()
-                                 .map(|triangle| {
-                                     let color = Color::random();
-                                     Triangle::new(Vertex::new(&triangle.a, &color),
-                                                   Vertex::new(&triangle.b, &color),
-                                                   Vertex::new(&triangle.c, &color))
-                                 })
-                                 .points(),
-                             UCube::with_unit_width()
-                                 .triangulate()
-                                 .points()
-                                 .enumerate()
-                                 .map(|(index, _)| index as Index));
-                buffer.append(&mut local);
+                buffer.extend(UCube::with_unit_width()
+                                  .map_points(|point| leaf.geometry.map_unit_cube_point(&point))
+                                  .map_points(|point| (point * width) + origin)
+                                  .triangulate()
+                                  .map(|triangle| {
+                                      let color = Color::random();
+                                      Triangle::new(Vertex::new(&triangle.a, &color),
+                                                    Vertex::new(&triangle.b, &color),
+                                                    Vertex::new(&triangle.c, &color))
+                                  })
+                                  .points(),
+                              UCube::with_unit_width()
+                                  .triangulate()
+                                  .points()
+                                  .enumerate()
+                                  .map(|(index, _)| index as Index));
             }
-        }
-        buffer
+            buffer
+        })
     }
 }
