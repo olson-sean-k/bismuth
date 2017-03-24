ba4c0ae4eac65c55e3c9ed9f04a573042dc90fc4
Sean Olson <olson.sean.k@gmail.com>
2017-03-24 13:18:11 -0700

    Limit `from_file` to 8-bit, normalized, unsigned RGBA formats.

--- src/render/texture.rs
+++ src/render/texture.rs
@@ -1,9 +1,18 @@
 use gfx::{self, Factory, Resources};
-use gfx::format::{Rgba8, TextureFormat};
+use gfx::format::{R8_G8_B8_A8, Rgba8, Srgb, TextureChannel, TextureFormat, Unorm};
 use gfx::handle::{Sampler, ShaderResourceView};
 use gfx::texture::{AaMode, FilterMethod, Kind, SamplerInfo, WrapMode};
 use image;
 use std::path::Path;
+
+pub trait NormalizedChannel {}
+pub trait UnsignedChannel {}
+
+impl NormalizedChannel for Srgb {}
+impl NormalizedChannel for Unorm {}
+
+impl UnsignedChannel for Srgb {}
+impl UnsignedChannel for Unorm {}
 
 #[derive(Clone)]
 pub struct Texture<R, T>
@@ -15,9 +24,10 @@ pub struct Texture<R, T>
     pub sampler: Sampler<R>,
 }
 
-impl<R, T> Texture<R, T>
+impl<R, C> Texture<R, (R8_G8_B8_A8, C)>
     where R: Resources,
-          T: TextureFormat
+          C: NormalizedChannel + TextureChannel + UnsignedChannel,
+          (R8_G8_B8_A8, C): TextureFormat
 {
     pub fn from_file<F, P>(factory: &mut F, path: P) -> Self
         where F: Factory<R>,
@@ -26,7 +36,7 @@ impl<R, T> Texture<R, T>
         // TODO: Return a `Result` and expose any errors from `image::open`.
         let data = image::open(path).unwrap().to_rgba();
         let (width, height) = data.dimensions();
-        let (surface, view) = factory.create_texture_immutable_u8::<T>(
+        let (surface, view) = factory.create_texture_immutable_u8::<(R8_G8_B8_A8, C)>(
             Kind::D2(width as u16, height as u16, AaMode::Single),
             &[data.into_vec().as_slice()]).unwrap();
         Texture {
