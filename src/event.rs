cee174590aa773f03c0e24dd41a74e6e8559b037
Sean Olson <olson.sean.k@gmail.com>
2017-05-01 15:59:56 -0700

    Require exclusive access to use `poll_events`.

--- src/event.rs
+++ src/event.rs
@@ -6,14 +6,14 @@ pub use winit::{ElementState, Event, MouseButton, MouseCursor, MouseScrollDelta,
 pub trait PollEvents {
     type Output: IntoIterator<Item = Event>;
 
-    fn poll_events(&self) -> Self::Output;
+    fn poll_events(&mut self) -> Self::Output;
 }
 
 impl PollEvents for Window {
     type Output = Vec<Event>;
 
-    fn poll_events(&self) -> Self::Output {
-        self.poll_events().collect()
+    fn poll_events(&mut self) -> Self::Output {
+        (&*self).poll_events().collect()
     }
 }
 
