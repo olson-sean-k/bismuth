27cc5208ded182d6ec5eddf078f39f6d3b75c0b4
Sean Olson <olson.sean.k@gmail.com>
2017-07-05 14:28:29 -0700

    Add documentation for `input` module.

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,3 +1,12 @@
+//! This module provides an interface for querying user input devices like
+//! keyboards and mice.
+//!
+//! Input devices track two stages of state: live state and snapshot state.
+//! Live state and snapshot state are updated via the `React` and `Snapshot`
+//! traits, respectively. This module provides access to the live state to
+//! query the current state of a device and also provides traits that compare
+//! live state to snapshot state.
+
 mod keyboard;
 mod mouse;
 mod state;
